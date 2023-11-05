c1bd8a06782b399bb6b1a2b57b7c020ecc442a67
Dulitha Harasara <129366975+DulithaHarasara@users.noreply.github.com>
2023-11-05 10:55:03 +0530

    Build user profile model

--- RaisingMinds/user_profile/models.py
+++ RaisingMinds/user_profile/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from users.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    profile_image = models.ImageField(upload_to='profile_images')
+
+    def __str__(self):
+        return self.user.username
+    
